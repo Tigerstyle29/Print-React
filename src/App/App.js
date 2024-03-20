d1a6c65de857eee35d9e24260a091e18b70d1907
Tigran Vartanyan <tvartanyan29@yandex.ru>
2024-03-20 15:06:18 +0400

    Add all in Git

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SquareCalculator from '../components/SquareCalculator/SquareCalculator';
+import ColorCalculator from '../components/ColorCalculator/ColorCalculator';
+
+function App() {
+  return (
+    <>
+      <SquareCalculator /> 
+      <ColorCalculator />
+    </>
+  );
+}
+
+export default App
